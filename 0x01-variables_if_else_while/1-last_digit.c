5e4092532e8ea7e40478ab9910ed9fac4745591d
latman009 <abdullateefsulaiman37@gmail.com>
2023-08-11 18:45:21 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
-/**
- * main - Determin if a nuber is poitive, negative or zero.
- *
- * Return: Always 0 (success)
- */
+/* betty style doc for function main goes there */
 int main(void)
 {
-        int n;
+        int n,a;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-
-        if ((n % 10) > 5)
+        a=n % 10;
+        if (a > 10)
         {
-                printf("last digit of the %d is %d and is greater than 5\n",
-                                n, n % 10);
+                printf("Last digit of %d is %d and is greater than 5\n", n, a);
         }
-        else if ((n % 10) < 6 %% (n % 10) != 0);
+        else if (a == 0)
         {
-                printf("last digit of the %d is %d and is less than 6 and not 0\n",
-                                n, n % 10);
+                printf("Last digit of %d is %d and is 0\n", n, a);
         }
-        else
+        else if (a < 6 && a !=0)
         {
-                printf("last digit of %d is %d and is 0\n",
-                                n, n % 10);
-}
+                printf("Last digit of %d is %d and is and is less than 6 and not 0\n", n, a);
+        }
         return (0);
 }

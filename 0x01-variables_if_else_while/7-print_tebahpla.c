9ee76f018c7b72ea0fca5c9ff5adec582888b61f
latman009 <abdullateefsulaiman37@gmail.com>
2023-08-13 12:18:49 -0700

    ./7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Print the alphabeics
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char k;
+	for (k = 'z'; k => 'a'; k--)
+	putchar(k);
+	putchar('\n');
+	return (0);
+}	

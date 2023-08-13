d79095ba5ff7b0dc6d55c743a90f03e163b1207d
latman009 <abdullateefsulaiman37@gmail.com>
2023-08-13 07:32:13 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry poit 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		int a;
+		for (a = 0; a < 10; a++)
+		{
+		printf("%d \n", a);
+		}
+		return (0);
+	}
+

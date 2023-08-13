d0aadfff800e08c034723b1b86dce38b60ecee40
latman009 <abdullateefsulaiman37@gmail.com>
2023-08-13 12:55:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Print the alphabeics
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a;
+	for (a = '0'; a <= '9'; a++)
+	{
+	putchar(a);
+	if (a != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
